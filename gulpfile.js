572cd75d4058bebfe472db5210d00e090af201b3
Damaal <93940560+Damaal@users.noreply.github.com>
2022-07-20 09:45:15 +0200

    modified:   build/css/app.css 	modified:   gulpfile.js 	modified:   index.html 	modified:   node_modules/.package-lock.json 	new file:   node_modules/ansi-cyan/LICENSE 	new file:   node_modules/ansi-cyan/index.js 	new file:   node_modules/ansi-cyan/package.json 	new file:   node_modules/ansi-cyan/readme.md 	new file:   node_modules/ansi-red/LICENSE 	new file:   node_modules/ansi-red/index.js 	new file:   node_modules/ansi-red/package.json 	new file:   node_modules/ansi-red/readme.md 	new file:   node_modules/ansi-styles/index.js 	new file:   node_modules/ansi-styles/license 	new file:   node_modules/ansi-styles/package.json 	new file:   node_modules/ansi-styles/readme.md 	new file:   node_modules/chalk/index.js 	new file:   node_modules/chalk/license 	new file:   node_modules/chalk/package.json 	new file:   node_modules/chalk/readme.md 	new file:   node_modules/escape-string-regexp/index.js 	new file:   node_modules/escape-string-regexp/license 	new file:   node_modules/escape-string-regexp/package.json 	new file:   node_modules/escape-string-regexp/readme.md 	new file:   node_modules/gulp-plumber/LICENSE 	new file:   node_modules/gulp-plumber/README.md 	new file:   node_modules/gulp-plumber/index.js 	new file:   node_modules/gulp-plumber/node_modules/arr-diff/LICENSE 	new file:   node_modules/gulp-plumber/node_modules/arr-diff/README.md 	new file:   node_modules/gulp-plumber/node_modules/arr-diff/index.js 	new file:   node_modules/gulp-plumber/node_modules/arr-diff/package.json 	new file:   node_modules/gulp-plumber/node_modules/arr-union/LICENSE 	new file:   node_modules/gulp-plumber/node_modules/arr-union/README.md 	new file:   node_modules/gulp-plumber/node_modules/arr-union/index.js 	new file:   node_modules/gulp-plumber/node_modules/arr-union/package.json 	new file:   node_modules/gulp-plumber/node_modules/array-slice/LICENSE 	new file:   node_modules/gulp-plumber/node_modules/array-slice/README.md 	new file:   node_modules/gulp-plumber/node_modules/array-slice/index.js 	new file:   node_modules/gulp-plumber/node_modules/array-slice/package.json 	new file:   node_modules/gulp-plumber/node_modules/extend-shallow/LICENSE 	new file:   node_modules/gulp-plumber/node_modules/extend-shallow/README.md 	new file:   node_modules/gulp-plumber/node_modules/extend-shallow/index.js 	new file:   node_modules/gulp-plumber/node_modules/extend-shallow/package.json 	new file:   node_modules/gulp-plumber/node_modules/kind-of/README.md 	new file:   node_modules/gulp-plumber/node_modules/kind-of/index.js 	new file:   node_modules/gulp-plumber/node_modules/kind-of/package.json 	new file:   node_modules/gulp-plumber/node_modules/plugin-error/LICENSE 	new file:   node_modules/gulp-plumber/node_modules/plugin-error/README.md 	new file:   node_modules/gulp-plumber/node_modules/plugin-error/index.js 	new file:   node_modules/gulp-plumber/node_modules/plugin-error/package.json 	new file:   node_modules/gulp-plumber/package.json 	new file:   node_modules/has-ansi/index.js 	new file:   node_modules/has-ansi/license 	new file:   node_modules/has-ansi/package.json 	new file:   node_modules/has-ansi/readme.md 	new file:   node_modules/supports-color/index.js 	new file:   node_modules/supports-color/license 	new file:   node_modules/supports-color/package.json 	new file:   node_modules/supports-color/readme.md 	modified:   package-lock.json 	modified:   package.json 	modified:   src/scss/app.scss 	new file:   src/scss/base/_globales.scss 	modified:   src/scss/base/_index.scss 	new file:   src/scss/base/_mixins.scss 	new file:   src/scss/base/_normalize.scss 	modified:   src/scss/base/_variables.scss 	deleted:    src/scss/contenido/_contenido.scss 	deleted:    src/scss/contenido/_index.scss 	new file:   src/scss/layout/_header.scss 	new file:   src/scss/layout/_index.scss 	new file:   src/scss/layout/_video.scss

--- gulpfile.js
+++ gulpfile.js
@@ -1,25 +1,19 @@
 const { src, dest, watch } = require("gulp"); //Extraer funcionalidades de GULP en este archivo
 const sass = require("gulp-sass")(require("sass")); //importo sass desde el plugin de gulp-sass
+const plumber = require("gulp-plumber"); // Para no detener el workflow cada vez que tenga algun error de compilación.
 
 function css(done){
-    //IDENTIFICAR EL ARCHIVO SASS
-
-    //COMPILARLO
-
-    //GUARDARLO
-
-    src("src/scss/app.scss")
-        .pipe( sass() )
-        .pipe( dest("build/css") ); 
-
+    
+    src("src/scss/**/*.scss")//IDENTIFICAR EL ARCHIVO SASS
+        .pipe( plumber() ) // 
+        .pipe( sass() )//COMPILARLO
+        .pipe( dest("build/css") ); //GUARDARLO
     done(); //callback para gulp, tarea finalizada.
 }
 
 function dev(done){
-    watch("src/scss/app.scss", css); //qué archivo mirar y qué hacer cuando cambie.
+    watch("src/scss/**/*.scss", css); //qué archivo mirar y qué hacer cuando cambie.
     done(); ///callback para gulp, tarea finalizada.
 }
-
-
 exports.css = css;
 exports.dev = dev;
